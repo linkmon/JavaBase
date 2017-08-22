8477bb3d515fa0167505c8509adbd48452836f5c
ray <523123316@qq.com>
2017-08-22 11:59:00 +0800

    add

--- Hijava/src/com/cxl/javaone/one/Demo.java
+++ Hijava/src/com/cxl/javaone/one/Demo.java
@@ -35,6 +35,21 @@ public class Demo {
 		System.out.println("b1 && b3 :" + (b1 && b2)); //与
 		System.out.println("b1 || b3 :" + (b1 || b2)); //或
 		
+		//短路逻辑运算符
+		int aa = 5;//定义一个变量；
+        boolean bb = (aa<4)&&(aa++<10);
+        System.out.println("使用短路逻辑运算符的结果为"+bb);
+        System.out.println("aa的结果为"+aa);
+        System.out.println("逻辑或路测试");
+        boolean cc = (aa<4)||(aa++<10);
+        System.out.println("cc的结果为"+cc);
+        System.out.println("aa的结果为"+aa);
+		/*
+		 * 逻辑与短路，如果第一个位False，后面则不运算
+		 * 逻辑或短路，如果第一个为True,后面则不运算
+		 */
+		
+		
 	}
 
 }
